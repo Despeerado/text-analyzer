f964a69b3482e56b73bbe4f7ca62164284b90b7b
Dominik Beran <d.beran27@gmail.com>
2023-06-29 12:50:15 +0200

    regular expressions

--- text_analysis.py
+++ text_analysis.py
@@ -1,31 +1,3 @@
-# text-analysis.py: první projekt do Engeto Online Python Akademie
-
-# author: Dominik Beran
-# discord: Dominik B. / Despeerado#8409
-# email: d.beran27@gmail.com
-
-# Vyžádá si od uživatele přihlašovací jméno a heslo,
-# zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů,
-# pokud je registrovaný, pozdrav jej a umožni mu analyzovat texty,
-# pokud není registrovaný, upozorni jej a ukonči program.**
-
-# Program nechá uživatele vybrat mezi třemi texty, uloženými v proměnné TEXTS:
-
-#     Pokud uživatel vybere takové číslo textu, které není v zadání, program jej upozorní a skončí,
-#     pokud uživatel zadá jiný vstup než číslo, program jej rovněž upozorní a skončí.
-
-# Pro vybraný text spočítá následující statistiky:
-
-#     počet slov,
-#     počet slov začínajících velkým písmenem,
-#     počet slov psaných velkými písmeny,
-#     počet slov psaných malými písmeny,
-#     počet čísel(ne cifer),
-#     sumu všech čísel(ne cifer) v textu.
-
-# Program zobrazí jednoduchý sloupcový graf,
-# který bude reprezentovat četnost různých délek slov v textu.
-
 from task_template import TEXTS
 # users
 
@@ -44,6 +16,11 @@ if users.get(user_name) == user_password:
     print("We have 3 texts to be analyzed.")
     text = input("Enter a number btw. 1 and 3 to select:")
 
+# Tuto cast jsem  opravil jen castecne, i kdyz jsem ji mel zmenit "pri takto nizkem poctu voleb bych sel radeji cestou
+# "if text in ["1", "2", "3"]" - potom nemusis kontrolovat rozsah a ani format"
+# duvodem je pozadovana operace v zadani - bud "input je jine cislo –> upozorni uzivatele a ukonci program",
+# nebo "vstup je neciselny znak -> upozorni uzivatele a ukonci program".
+
     if text.isdigit():
         index = int(text)
         if 1 <= index <= 3:
@@ -51,10 +28,11 @@ if users.get(user_name) == user_password:
 
             # removing punctuation
 
-            punctuation_marks = [".", ",", "!", "?", ":"]
+            # punctuation_marks = [".", ",", "!", "?", ":", "\n"]
+            punctuation_marks = r"[., ?!:\n]"
 
             for mark in punctuation_marks:
-                element = element.replace(mark, "")
+                element = element.replace(mark, " ")
             words = element.split()
             word_count = len(words)
             print("There are", word_count, "words in the selected text.")
